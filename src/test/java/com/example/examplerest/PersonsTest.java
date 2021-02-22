4ac6e06ea8981890d00cd446457c6fe66338e879
unknown <jcalle.alamo@gmail.com>
2021-02-22 11:09:28 -0500

    Update8

---
+++ src/test/java/com/example/examplerest/PersonsTest.java
@@ -0,0 +1,17 @@
+package com.example.examplerest;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersonsTest {
+	
+	@Before
+    public void setUpClass() {
+		
+    }
+}
